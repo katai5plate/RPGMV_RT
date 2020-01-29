2db900f199c1667937f0d8717ac95f857e9051f1
katai5plate <katai5plate.sub@gmail.com>
2020-01-30 03:28:30 +0900

    delete plugins

--- dist/js/plugins.js
+++
@@ -1,1 +0,0 @@
-var $plugins=[{"name":"Community_Basic","status":true,"description":"基本的なパラメーターを設定するプラグインです。","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}}]
