c1f28b53f23448536dbd9d492415ea65a3f6dce7
katai5plate <katai5plate.sub@gmail.com>
2020-02-01 15:55:34 +0900

    add indexWithCss

--- scripts/generateHTML.js
+++ scripts/generateHTML.js
@@ -1,10 +1,15 @@
 const { writeFileSync, readFileSync } = require("fs");
-const [, , dist] = process.argv;
+const [, , dist, enableCSS] = process.argv;
 
 const title = "";
 const styleCode = readFileSync("./temp/gamefont.conv.css");
 const lzCode = readFileSync("./temp/lz.min.js");
 const mvCode = readFileSync("./temp/mv.lz.js");
+
+const cssCode =
+  enableCSS === "css"
+    ? '<link rel="stylesheet" type="text/css" href="index.css"/>'
+    : `<style>${styleCode}</style>`;
 
 writeFileSync(
   dist,
@@ -16,7 +21,7 @@ writeFileSync(
     ' content="black-translucent">',
     '<meta name="viewport" content="user-scalable=no">',
     '<link rel="icon" href="icon/icon.png" type="image/png"/>',
-    `<style>${styleCode}</style><title>${title}</title></head>`,
+    `${cssCode}<title>${title}</title></head>`,
     '<body style="background-color:#000">',
     `<script>${lzCode}</script><script>${mvCode}</script>`,
     '<script type="text/javascript" src="js/plugins.js"></script>',
